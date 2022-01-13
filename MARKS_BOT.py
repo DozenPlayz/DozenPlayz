94d58bd86031275b9232f19ae5e95c5e27baf96d
DozenPlayzYT <97524846+DozenPlayz@users.noreply.github.com>
2022-01-13 15:33:07 +0530

    Create MARKS_BOT.py

---
+++ MARKS_BOT.py
@@ -0,0 +1,9 @@
+submaths = int(input("Enter Marks For Math: "))
+subenglish = int(input("Enter Marks For English: "))
+SubSST = int(input("Enter Marks For SST: "))
+
+if subenglish>= 33 and  submaths>= 33 and SubSST>= 33:
+    print("Pass")
+else:
+    print("Fail")
+    
