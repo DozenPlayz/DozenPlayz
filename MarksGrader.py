c12f78d9214577805cdc204f3e3144fd595c60f0
DozenPlayzYT <97524846+DozenPlayz@users.noreply.github.com>
2022-01-13 19:17:49 +0530

    Update MarksGrader.py

--- MarksGrader.py
+++ MarksGrader.py
@@ -1,14 +1,14 @@
 grade = int(input("Enter Marks: "))
 
-if grade>=90 and grade<=100:
+if grade>=90
     print("Ex")
-elif grade>=80 and grade<=90:
+elif grade>=80
     print("A")
-elif grade>=70 and grade<=80:
+elif grade>=70
     print("B")
-elif grade>=60 and grade<=70:
+elif grade>=60
     print("C")
-elif grade>=50 and grade<=60:
+elif grade>=50
     print("D")
 else:
     print("F")
